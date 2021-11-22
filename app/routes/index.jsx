8b35c3f8d0a42381bb1f72e59b77aec6a5532f45
ronan-f <rfegan13@gmail.com>
2021-11-22 15:52:55 -0800

    Added stylesheet

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,3 +1,9 @@
+import stylesUrl from "../styles/index.css";
+
 export default function IndexRoute() {
   return <div>Hello from the index</div>;
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: stylesUrl }];
 }

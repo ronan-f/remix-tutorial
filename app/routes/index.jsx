8a05e34c8da4631bc0c06ccab4057a9cd18b611e
ronan-f <rfegan13@gmail.com>
2021-11-22 16:08:36 -0800

    Added styling

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,9 +1,30 @@
+import { Link } from "remix";
 import stylesUrl from "../styles/index.css";
 
-export default function IndexRoute() {
-  return <div>Hello from the index</div>;
-}
+export let links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
 
-export function links() {
-  return [{ rel: "stylesheet", href: stylesUrl }];
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
